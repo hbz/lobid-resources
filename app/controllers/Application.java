2e94eaac03ec0fc6f73ef93cf22a89cf2ccf405d
Fabian Steeg <fsteeg@gmail.com>
2014-01-30 16:57:11 +0100

    Set up basic communication with Lobid API
    - Configure API location and set specification in application.conf
    - Display config settings & sample query with result on index page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,44 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import com.typesafe.config.Config;
 
 public class Application extends Controller {
+	public final static Config CONFIG = com.typesafe.config.ConfigFactory
+			.parseFile(new java.io.File("conf/application.conf")).resolve();
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result index() {
+		final String query = "Buch";
+		final String url = url(query);
+		final String result = call(url);
+		return ok(index.render(CONFIG, query, url, result));
+	}
 
+	public static String url(String query) {
+		final String template = "%s/resource?set=%s&q=%s&format=short";
+		return String.format(template, CONFIG.getString("nwbib.api"),
+				CONFIG.getString("nwbib.set"), query);
+	}
+
+	public static String call(final String url) {
+		try {
+			final URLConnection connection = new URL(url).openConnection();
+			connection.setRequestProperty("Accept", "application/json");
+			return CharStreams.toString(new InputStreamReader(connection
+					.getInputStream(), Charsets.UTF_8));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,44 +1,35 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.contentType;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.junit.*;
+import org.junit.Test;
 
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
-
-import static play.test.Helpers.*;
-import static org.fest.assertions.Assertions.*;
-
+import play.mvc.Content;
+import controllers.Application;
 
 /**
-*
-* Simple (JUnit) tests that can call all parts of a play app.
-* If you are interested in mocking a whole application, see the wiki for more details.
-*
-*/
+ * 
+ * Simple (JUnit) tests that can call all parts of a play app. If you are
+ * interested in mocking a whole application, see the wiki for more details.
+ * 
+ */
 public class ApplicationTest {
 
-    @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
-    }
+	@Test
+	public void simpleCheck() {
+		int a = 1 + 1;
+		assertThat(a).isEqualTo(2);
+	}
 
-    @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
-        assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Your new application is ready.");
-    }
-
-
+	@Test
+	public void renderTemplate() {
+		String query = "Buch";
+		String url = Application.url(query);
+		Content html = views.html.index.render(Application.CONFIG, query, url,
+				Application.call(url));
+		assertThat(contentType(html)).isEqualTo("text/html");
+		String text = contentAsString(html);
+		assertThat(text).contains("nwbib.api").contains("nwbib.set")
+				.contains("Brühl im Buch");
+	}
 }
