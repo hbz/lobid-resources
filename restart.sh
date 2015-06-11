4ced144564992091ea93255fc7d4efa495178941
dr0i <christoph@hbz-nrw.de>
2015-06-11 11:04:06 +0200

    Add start/stop script which is also used by monit
    See hbz/lobid#157.
    
    This script is deliberately heavily parametrized since the old one
    could restart the production services when rolled out to staging.
    It's now generic enough to be used by all play instances we use
    (currently 4).

--- restart.sh
+++ restart.sh
@@ -1,1 +1,39 @@
-kill $(cat target/universal/stage/RUNNING_PID) ; ./activator-1.2.10-minimal/activator "start 8000"
+#!/bin/sh
+
+USAGE="<GIT REPO NAME> {start|stop} <PORT> [<JAVA OPTS>]"
+
+if [ $# -lt 3 ]; then
+	echo "$USAGE
+				First 3 parameters are mandatory.
+				Don't forget that the process is monitored by 'monit'.
+				It will restart automatically if you stop the API.
+				If you want to stop it permanently, do 'sudo /etc/ini.d/monit stop' first.
+				"
+	exit 65
+fi
+
+REPO=$1
+ACTION=$2
+PORT=$3
+JAVA_OPTS="$4"
+
+HOME="/home/sol"
+
+# it is important to set the proper locale
+. $HOME/.locale
+
+cd $HOME/git/$REPO
+case $ACTION in
+	start)
+		kill $(cat target/universal/stage/RUNNING_PID)
+		JAVA_OPTS="$JAVA_OPTS" $HOME/activator-1.2.10-minimal/activator "start $PORT"
+		;;
+	stop)
+		kill $(cat target/universal/stage/RUNNING_PID)
+		;;
+	*) 
+		echo "usage: $USAGE"
+		;;
+esac
+exit 0
+
