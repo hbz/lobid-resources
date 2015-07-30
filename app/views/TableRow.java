9d455ca51e5a70be47d260423d68e6eb5f877077
Fabian Steeg <fsteeg@gmail.com>
2015-07-30 16:49:22 +0200

    Move pattern matching over Java enum in Scala template into enum
    In preparation for changes as required for #105

--- app/views/TableRow.java
+++ app/views/TableRow.java
@@ -1,6 +1,18 @@
-/* Copyright 2014 Fabian Steeg, hbz. Licensed under the GPLv2 */
+/* Copyright 2014-2015 Fabian Steeg, hbz. Licensed under the GPLv2 */
 
 package views;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import controllers.nwbib.Application;
+import controllers.nwbib.Classification;
+import controllers.nwbib.Lobid;
+import play.api.mvc.Call;
 
 /**
  * Different ways of serializing a table row
@@ -10,5 +22,105 @@ package views;
 @SuppressWarnings("javadoc")
 /* no javadoc for elements. */
 public enum TableRow {
-	VALUES, LINKS
+
+	VALUES {
+		@Override
+		public String process(JsonNode doc, String property, String label,
+				List<String> values, Optional<List<String>> keys) {
+			return values.stream()
+					.filter(value -> !value.contains("http://dewey.info")
+							&& (!property.startsWith("contributor") || count(doc, value) < 3))
+					.map(val -> String.format("<tr><td>%s</td><td>%s</td></tr>", label,
+							label(doc, property, val, keys)))
+					.collect(Collectors.joining("\n"));
+		}
+
+		private int count(JsonNode doc, String id) {
+			int c = 0;
+			int i = -1;
+			while ((i = doc.toString().indexOf(id, i + 1)) > 0) {
+				c++;
+			}
+			return c;
+		}
+
+		private String label(JsonNode doc, String property, String value,
+				Optional<List<String>> labels) {
+			if (!labels.isPresent()) {
+				return refAndLabel(property, value)[0];
+			}
+			Call search = controllers.nwbib.routes.Application.search(
+					"\"" + value + "\"", "", "", "", "", "", "", "", "", "", 0, 10, "",
+					"", "", false, "", "", "");
+			return String.format(
+					"<a title=\"Nach weiteren Titeln suchen\" href=\"%s\">%s</a> | "
+							+ "<a title=\"Linked-Data-Quelle abrufen\" "
+							+ "href=\"%s\"><span class=\"glyphicon glyphicon-link\"></span></a>",
+					search, createLabel(doc, value, labels), value);
+		}
+
+		private String createLabel(JsonNode doc, String value,
+				Optional<List<String>> labels) {
+			String label = "";
+			if (value.startsWith("http://purl.org/lobid/nwbib")) {
+				label = String.format("%s (%s)", //
+						Application.CLASSIFICATION.ids(value, "").findValue("label")
+								.asText(),
+						Classification.shortId(value));
+			} else {
+				label = labelFor(doc, value, labels);
+			}
+			return label;
+		}
+
+		private String labelFor(JsonNode doc, String value,
+				Optional<List<String>> keys) {
+			if (!keys.isPresent() || keys.get().isEmpty()) {
+				return value;
+			}
+			List<String> result = new ArrayList<>();
+			for (JsonNode node : doc.get("@graph")) {
+				for (String key : keys.get()) {
+					if (node.get("@id").textValue().equals(value) && node.has(key)) {
+						result.add(node.get(key).textValue());
+					}
+				}
+			}
+			return result.isEmpty() ? value : result.get(0);
+		}
+	},
+	LINKS {
+		@Override
+		public String process(JsonNode doc, String property, String label,
+				List<String> values, Optional<List<String>> labels) {
+			return values.stream()
+					.filter(value -> !value.contains("lobid.org/resource/NWBib"))
+					.map(value -> String.format("<tr><td>%s</td><td>%s</td></tr>", label,
+							link(property, value)))
+					.collect(Collectors.joining("\n"));
+		}
+
+		private String link(String property, String val) {
+			String[] refAndLabel = refAndLabel(property, val);
+			String href = refAndLabel[0];
+			String label = refAndLabel[1];
+			return String.format("<a title='%s' href='%s'>%s</a>", href, href, label);
+		}
+
+	};
+
+	String[] refAndLabel(String property, String value) {
+		if (property.equals("subjectChain")) {
+			return new String[] { value.replaceAll("\\([\\d,]+\\)$", ""), "" };
+		} else if ((property.equals("isPartOf") || property.equals("hasPart"))
+				&& value.contains("lobid.org")) {
+			return new String[] {
+					value.replace("lobid.org/resource/", "lobid.org/nwbib/"),
+					Lobid.resourceLabel(value) };
+		}
+		return new String[] { value, value };
+	}
+
+	public abstract String process(JsonNode doc, String property, String label,
+			List<String> values, Optional<List<String>> labels);
 }
