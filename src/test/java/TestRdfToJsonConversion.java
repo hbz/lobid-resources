e71ef149c2689490ed55e29009b49ada0a446c42
Jan Schnasse <jan@schnasse.eu>
2015-11-30 16:45:13 +0100

    introduces object comparison with json library
    - uses equals method to perform unordered comparisons

--- src/test/java/TestRdfToJsonConversion.java
+++ src/test/java/TestRdfToJsonConversion.java
@@ -18,18 +18,19 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
 import org.openrdf.rio.RDFFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import de.hbz.lobid.helper.JsonConverter;
 
@@ -48,9 +49,9 @@ public class TestRdfToJsonConversion {
 	public void testEquality_case1()
 			throws JsonParseException, JsonMappingException, IOException {
 		boolean result = testFiles("adrianInput.nt", "hbz01.es.json",
-				"http://lobid.org/resource/HT018454638");
+				"http://lobid.org/resource/");
 		TestRdfToJsonConversion.logger
-				.info("\nEnd. Equality:" + false + ", expected equality:" + true);
+				.info("\n Adrian Input Test - must succeed! \n");
 		org.junit.Assert.assertTrue(result);
 	}
 
@@ -59,10 +60,9 @@ public class TestRdfToJsonConversion {
 	public void testEquality_case2()
 			throws JsonParseException, JsonMappingException, IOException {
 		boolean result = testFiles("input/nt/01877/HT018770176.nt",
-				"output/json/01877/HT018770176.json",
-				"http://lobid.org/resource/HT018770176");
+				"output/json/01877/HT018770176.json", "http://lobid.org/resource/");
 		TestRdfToJsonConversion.logger
-				.info("\nEnd. Equality:" + false + ", expected equality:" + true);
+				.info("\n Blank Node Input Test - must succeed! \n");
 		org.junit.Assert.assertTrue(result);
 	}
 
@@ -74,14 +74,14 @@ public class TestRdfToJsonConversion {
 				testFiles("adrianInput.nt", "hbz01.es.wrongContributorOrder.json",
 						"http://lobid.org/resource/HT018454638");
 		TestRdfToJsonConversion.logger
-				.info("\nEnd. Equality:" + false + ", expected equality:" + false);
+				.info("\n WrongContributorOrder Test - must fail! \n");
 		org.junit.Assert.assertFalse(result);
 	}
 
 	private boolean compare(final Map<String, Object> expected,
 			final Map<String, Object> actual) {
 		for (final String s : expected.keySet()) {
-			TestRdfToJsonConversion.logger.debug("try to get " + s);
+			TestRdfToJsonConversion.logger.trace("try to get " + s);
 			if (!compareObjects(expected.get(s), actual.get(s))) {
 				return false;
 			}
@@ -91,41 +91,49 @@ public class TestRdfToJsonConversion {
 
 	@SuppressWarnings("unchecked")
 	private boolean compareObjects(final Object expected, final Object actual) {
-
 		boolean result = false;
-
 		if (actual instanceof String) {
-			TestRdfToJsonConversion.logger.debug("Expected: " + expected);
-			TestRdfToJsonConversion.logger.debug("Actual: " + actual);
+			TestRdfToJsonConversion.logger.debug("Compare String");
 			result = expected.equals(actual);
-			if (!result) {
-				warn(expected, actual);
-			}
-		} else if (actual instanceof Set) {
-			// not order sensitive
-			result = compare((ArrayList<?>) expected, (Set<Object>) actual);
 		} else if (actual instanceof List) {
+			TestRdfToJsonConversion.logger.debug("Compare List");
 			// order sensitive
 			result = expected.toString().equals(actual.toString());
-			if (!result) {
-				warn(expected, actual);
-			}
 		} else if (actual instanceof Map) {
+			TestRdfToJsonConversion.logger.debug("Compare Object");
 			result =
 					compare((Map<String, Object>) expected, (Map<String, Object>) actual);
+		} else if (actual instanceof Set) {
+			TestRdfToJsonConversion.logger.debug("Compare Set");
+			// order insensitive
+			result = compareUnordered((List) expected, (Set) actual);
+		}
+		if (!result) {
+			warn(expected, actual);
 		}
 		return result;
 	}
 
-	private static boolean compare(final ArrayList<?> expected,
+	/**
+	 * 
+	 * @param expected the expected value comes from json data. all json arrays
+	 *          will be converted to lists by jackson object mapper.
+	 * @param actual the actual value comes from n-triples. The JsonConverter uses
+	 *          sets to express multiple occurrences of objects. The datatype list
+	 *          is reserved for rdf-lists.
+	 * @return
+	 */
+	private static boolean compareUnordered(final List<Object> expected,
 			final Set<Object> actual) {
-		return expected.size() == actual.size();
+		ObjectMapper mapper = new ObjectMapper();
+		ArrayNode newExpected = mapper.convertValue(expected, ArrayNode.class);
+		ArrayNode newActual = mapper.convertValue(actual, ArrayNode.class);
+		return newExpected.equals(newActual);
 	}
 
 	private static void warn(Object expected, Object actual) {
-		logger.warn("Order sensitive comparison will fail - Not Equal: \n'"
-				+ expected + "'\n'" + actual + "'\n" + expected.hashCode() + "\n"
-				+ actual.hashCode());
+		logger.warn("Comparison will fail - Not Equal: \n'" + expected + "'\n'"
+				+ actual + "'\n");
 	}
 
 	private boolean testFiles(String fnameNtriples, String fnameJson, String uri)
@@ -146,6 +154,7 @@ public class TestRdfToJsonConversion {
 							+ fnameJson + ", expect equality:" + false);
 			final Map<String, Object> expected =
 					new ObjectMapper().readValue(out, Map.class);
+
 			return compareObjects(expected, actual);
 		}
 	}
