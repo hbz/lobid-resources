e4eea9cfee6236a1f8d22ff91d9ebdc74f55ea6a
Jan Schnasse <jan@schnasse.eu>
2015-12-18 16:29:36 +0100

    use existing data for comparison

--- src/test/java/TestJsonToRdfConversion.java
+++ src/test/java/TestJsonToRdfConversion.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -31,92 +32,122 @@ import de.hbz.lobid.helper.RdfUtils;
  */
 public class TestJsonToRdfConversion {
 	private static final String BASE = "src/test/resources/";
-	private static final String REVERSE_IN = "reverseTest/input/json/";
+	private static final String REVERSE_IN = "output/json/";
 	private static final String REVERSE_OUT = "reverseTest/output/nt/";
 	private static final String TEST_IN = "input/nt/";
-	private static final String TEST_OUT = "output/json/";
-	private static final String TEST_PATH = "src/test/resources/input/nt/";
 	final static Logger logger =
 			LoggerFactory.getLogger(TestJsonToRdfConversion.class);
+	private static final boolean PRINT = false;
 
 	@SuppressWarnings({ "javadoc" })
 	@Test
 	public void test_all() {
-
 		try {
 			Files.walk(Paths.get(BASE + REVERSE_IN)).filter(Files::isRegularFile)
 					.forEach(path -> {
-						String jsonFilename = getRelativePath(BASE + REVERSE_IN, path);
-						String rdfFilename = getRelativePath(BASE + REVERSE_IN, path)
-								.replaceFirst("json$", "nt");
-
-						compare(jsonFilename, rdfFilename);
+						test(path);
 					});
 		} catch (Exception e) {
-			e.printStackTrace();
-			org.junit.Assert.assertFalse(true);
+			throw new RuntimeException(e);
 		}
 	}
 
-	private String getRelativePath(String relpath, Path path) {
-		return new File(relpath).toURI().relativize(path.toUri()).getPath();
-	}
-
-	void compare(String jsonFilename, String rdfFilename) {
+	private void test(Path path) {
 		try {
-			logger.info(jsonFilename + " " + rdfFilename);
-			String jsonString = createJsonString(REVERSE_IN + jsonFilename);
-			String rdfString = RdfUtils.readRdfToString(
-					new ByteArrayInputStream(jsonString.getBytes("utf-8")),
-					RDFFormat.JSONLD, RDFFormat.NTRIPLES, "");
-			// writeToFileIfNotExists(rdfString, REVERSE_OUT + rdfFilename);
-			org.junit.Assert
-					.assertTrue(rdfCompare(rdfString, getExpected(rdfFilename)));
+			String jsonFilename = getRelativePath(BASE + REVERSE_IN, path);
+			String rdfFilename = getRelativePath(BASE + REVERSE_IN, path);
+			rdfFilename = rdfFilename.replaceFirst("json$", "nt");
+			compare(jsonFilename, rdfFilename);
 		} catch (Exception e) {
 			logger.error("", e);
 		}
 	}
 
-	private String getExpected(String rdfFilename) {
-		logger.debug("Read rdf " + TEST_IN + rdfFilename);
+	private static String getRelativePath(String relpath, Path path) {
+		return new File(relpath).toURI().relativize(path.toUri()).getPath();
+	}
 
+	void compare(String jsonFilename, String rdfFilename) {
+		try {
+			logger.info("Compare: " + jsonFilename + " " + rdfFilename);
+			String actualRdfString = getActual(jsonFilename);
+			String expectedRdfString = getExpected(rdfFilename);
+			if (PRINT) {
+				writeToFileIfNotExists(actualRdfString, REVERSE_OUT + rdfFilename);
+			}
+			boolean stringsAreEqual = rdfCompare(actualRdfString, expectedRdfString);
+			org.junit.Assert.assertTrue(stringsAreEqual);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static String getActual(String jsonFilename)
+			throws UnsupportedEncodingException {
+		String jsonString = createJsonString(REVERSE_IN + jsonFilename);
+		String actualRdfString = RdfUtils.readRdfToString(
+				new ByteArrayInputStream(jsonString.getBytes("utf-8")),
+				RDFFormat.JSONLD, RDFFormat.NTRIPLES, "");
+		return actualRdfString;
+	}
+
+	private static String getExpected(String rdfFilename) {
+		logger.debug("Read rdf " + TEST_IN + rdfFilename);
 		try (InputStream in = Thread.currentThread().getContextClassLoader()
 				.getResourceAsStream(TEST_IN + rdfFilename)) {
-
 			String rdfString = RdfUtils.readRdfToString(in, RDFFormat.NTRIPLES,
 					RDFFormat.NTRIPLES, "");
-
 			return rdfString;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	private boolean rdfCompare(String actual, String expected) {
+	private static boolean rdfCompare(String actual, String expected) {
 		String actualWithoutBlankNodes = removeBlankNodes(actual);
 		String expectedWithoutBlankNodes = removeBlankNodes(expected);
-		String actualSorted = sorted(actualWithoutBlankNodes);
-		String expectedSorted = sorted(expectedWithoutBlankNodes);
+		String[] actualSorted = sorted(actualWithoutBlankNodes);
+		String[] expectedSorted = sorted(expectedWithoutBlankNodes);
+		printErrors(actualSorted, expectedSorted);
 		return actualSorted.equals(expectedSorted);
 	}
 
-	private String removeBlankNodes(String str) {
+	private static void printErrors(String[] actualSorted,
+			String[] expectedSorted) {
+		for (int i = 0; i < actualSorted.length; i++) {
+			if (actualSorted[i].equals(expectedSorted[i])) {
+				logger.debug("Actual , Expected");
+				logger.debug(actualSorted[i]);
+				logger.debug(expectedSorted[i]);
+				logger.debug("");
+			} else {
+				logger.error("Error line " + i);
+				logger.error("Actual , Expected");
+				logger.error(actualSorted[i]);
+				logger.error(expectedSorted[i]);
+				logger.error("");
+			}
+		}
+	}
+
+	private static String removeBlankNodes(String str) {
 		return str.replaceAll("_:[^\\ ]*", "")
 				.replaceAll("\\^\\^<http://www.w3.org/2001/XMLSchema#string>", "");
 	}
 
-	private String sorted(String actualWithoutBlankNodes) {
+	private static String[] sorted(String actualWithoutBlankNodes) {
 		String[] list = createList(actualWithoutBlankNodes);
-		ArrayList<String> words = new ArrayList<String>(Arrays.asList(list));
+		ArrayList<String> words = new ArrayList<>(Arrays.asList(list));
 		Collections.sort(words, String.CASE_INSENSITIVE_ORDER);
 		logger.debug(words.toString());
-		return words.toString();
+		String[] ar = new String[words.size()];
+		return words.toArray(ar);
 	}
 
-	private String[] createList(String actualWithoutBlankNodes) {
+	private static String[] createList(String actualWithoutBlankNodes) {
 		try (BufferedReader br =
 				new BufferedReader(new StringReader(actualWithoutBlankNodes))) {
-			List<String> result = new ArrayList<String>();
+			List<String> result = new ArrayList<>();
 			String line;
 			while ((line = br.readLine()) != null) {
 				result.add(line);
@@ -128,10 +159,9 @@ public class TestJsonToRdfConversion {
 		}
 	}
 
-	private String createJsonString(String jsonFilename) {
+	private static String createJsonString(String jsonFilename) {
 		try {
-			logger.info("Read json " + jsonFilename);
-			// String jsonString = readFileFromClasspath(fileName);
+			logger.debug("Read json " + jsonFilename);
 			Map<String, Object> jsonMap = JsonConverter.getObjectMapper()
 					.readValue(Thread.currentThread().getContextClassLoader()
 							.getResourceAsStream(jsonFilename), Map.class);
@@ -147,17 +177,11 @@ public class TestJsonToRdfConversion {
 	private static void writeToFileIfNotExists(String rdf, String fileName)
 			throws URISyntaxException, FileNotFoundException, IOException {
 		try {
-			File rdfFile = Paths.get(Thread.currentThread().getContextClassLoader()
+			Paths.get(Thread.currentThread().getContextClassLoader()
 					.getResource(fileName).toURI()).toFile();
 		} catch (NullPointerException e) {
 			makeFile(rdf, fileName);
 		}
-	}
-
-	private static String readFileFromClasspath(final String fileName)
-			throws IOException, URISyntaxException {
-		return new String(Files.readAllBytes(Paths.get(Thread.currentThread()
-				.getContextClassLoader().getResource(fileName).toURI())));
 	}
 
 	private static void makeFile(String rdf, String fileName) throws IOException {
