1303bee620fe7cbbead969e442934ffa90db0ecf
dr0i <christoph@hbz-nrw.de>
2016-02-17 09:48:06 +0100

    Check if test file is XML
    If a resource that is in the list "testIds.txt" is removed from the catalog
    it can no more be get via http://lobid.org/resource?id=$id&format=source.
    The result of such a lookup is "No source data found for request" which mustn't
    be saved as an XML file because that parsing will result in:
     org.xml.sax.SAXParseException; lineNumber: 1; columnNumber: 1; Content is not allowed in prolog
    This sanity check makes sure that empty resources are not stored.
    
    - the repo's directory is no more left to avoid confusion
    
    See https://github.com/hbz/lobid-resources/pull/23#issuecomment-184697253.

--- buildAndETLTestSet.sh
+++ buildAndETLTestSet.sh
@@ -8,7 +8,7 @@
 # will be fast.
 
 # Adapt this path to your needs. Avoid /tmp and rebooting, see above.
-TARGET=~/lobidTestResources
+TARGET=tmpTestFiles
 TEST_FILE="test.tar.bz2"
 THIS="$(pwd)"
 BRANCH=$1
@@ -28,10 +28,16 @@ tar xfj $THIS/src/test/resources/hbz01XmlClobs.tar.bz2
 cd -
 # include list
 for i in $(cat testIds.txt); do
-        if [ ! -f $TARGET/$i ]
-                then curl "http://lobid.org/resource?id=$i&format=source" > $TARGET/$i
-                echo "Added new resource $i"
-        fi
+	if [ ! -f $TARGET/$i ]; then
+		curl -s "http://lobid.org/resource?id=$i&format=source" > $TARGET/$i
+		grep "^<?xml" $TARGET/$i 2>&1 >/dev/null
+		if [ $? -eq 0 ]; then
+			echo "Adding new resource $i"
+			else
+				echo "Ignoring $TARGET/$i, don't seems to be an XML file"
+				rm $TARGET/$i
+			fi
+		fi
 done
 
 rm $TEST_FILE
