3427a56f88232973e2e4d91f21fe917a4397c576
dr0i <christoph@hbz-nrw.de>
2016-03-01 14:04:04 +0100

    Use jitpack to handle lobid-rdf-to-json library
    - add jitpack repository to pom
    - add lobid-rdf-to-json dependency to pom
    - remove git building lobid-rdf-to-json from script
    
    Resolves hbz/lobid-rdf-to-json#32.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,8 +4,3 @@ git clone https://github.com/hbz/metafacture-core.git
 cd metafacture-core
 git pull
 mvn clean install -DskipTests=true
-cd ..
-git clone https://github.com/hbz/lobid-rdf-to-json.git
-cd lobid-rdf-to-json
-git pull
-mvn clean install -DskipTests=true
