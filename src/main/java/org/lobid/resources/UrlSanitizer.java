eecf358083cf8bd46bac957b2506fe4f542524b4
dr0i <christoph@hbz-nrw.de>
2016-06-23 15:10:30 +0200

    Rename EnforceUrl to UrlSanitizer
    - add UrlValidator
    - add test resource HT017411546
    - add test resource HT006698544
    - update tests
    
    See #85 and https://github.com/hbz/lobid-resources/pull/87

---
+++ src/main/java/org/lobid/resources/UrlSanitizer.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright 2016 hbz, Pascal Christoph
+ *
+ *  Licensed under the Apache License, Version 2.0 the "License";
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.lobid.resources;
+
+import org.apache.commons.validator.routines.UrlValidator;
+import org.culturegraph.mf.morph.functions.AbstractSimpleStatelessFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Checks if a string is a valid absolute URL. If it is not, try to repair the
+ * URL. Returns an empty string if it can't be repaired.
+ * 
+ * @author Pascal Christoph (dr0i)
+ */
+public final class UrlSanitizer extends AbstractSimpleStatelessFunction {
+	private static final Logger LOG = LoggerFactory.getLogger(UrlSanitizer.class);
+	static UrlValidator urlValidator = new UrlValidator();
+
+	@Override
+	public String process(final String value) {
+		return sanitizeUrl(value);
+	}
+
+	private static String sanitizeUrl(final String value) {
+		String url = value.trim();
+		if (!urlValidator.isValid(url)) {
+			url = url.replace(" ", "%20");// space in URI
+			if (!urlValidator.isValid(url)) {
+				for (String urlSplitter : url.split("%20")) {
+					if (urlValidator.isValid(urlSplitter))
+						return urlSplitter;
+				}
+				// assuming scheme is missing
+				url = "http://" + url;
+				if (!urlValidator.isValid(url)) {
+					url = "";
+					LOG.info("No absolute URI could be generated from '" + value);
+				}
+			}
+		}
+		return url;
+	}
+}
