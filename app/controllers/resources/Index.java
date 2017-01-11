b6b776fcf8b4c8e74644e2a40af6a80ccfe6929f
Fabian Steeg <fsteeg@gmail.com>
2017-01-11 17:47:37 +0100

    Extract index config to resources.conf file (see #14)

--- app/controllers/resources/Index.java
+++ app/controllers/resources/Index.java
@@ -21,17 +21,29 @@ import play.libs.Json;
  */
 public class Index {
 
+	private static final String INDEX_NAME =
+			Application.CONFIG.getString("index.name");
+	private static final String TYPE_ITEM =
+			Application.CONFIG.getString("index.type.item");
+	private static final int CLUSTER_PORT =
+			Application.CONFIG.getInt("index.cluster.port");
+	private static final String CLUSTER_HOST =
+			Application.CONFIG.getString("index.cluster.host");
+	private static final String CLUSTER_NAME =
+			Application.CONFIG.getString("index.cluster.name");
+
 	/**
 	 * @param id The item ID
 	 * @return The item JSON
 	 */
 	public static JsonNode getItem(String id) {
+
 		Settings settings =
-				Settings.settingsBuilder().put("cluster.name", "gaia-aither").build();
+				Settings.settingsBuilder().put("cluster.name", CLUSTER_NAME).build();
 		try (Client client = TransportClient.builder().settings(settings).build()
 				.addTransportAddress(new InetSocketTransportAddress(
-						InetAddress.getByName("aither.hbz-nrw.de"), 9300))) {
-			GetResponse response = client.prepareGet("resources", "item", id)
+						InetAddress.getByName(CLUSTER_HOST), CLUSTER_PORT))) {
+			GetResponse response = client.prepareGet(INDEX_NAME, TYPE_ITEM, id)
 					.setParent(id.split(":")[0]).execute().actionGet();
 			return Json.parse(response.getSourceAsString());
 		} catch (UnknownHostException e) {
