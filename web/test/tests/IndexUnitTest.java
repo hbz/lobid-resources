510dff0c17479d7f5f111479f2018f2e186c2dca
Fabian Steeg <fsteeg@gmail.com>
2017-02-03 14:43:18 +0100

    Support ISSN in advanced search, display ISSN in details
    See https://github.com/hbz/lobid-resources-web/issues/34

--- web/test/tests/IndexUnitTest.java
+++ web/test/tests/IndexUnitTest.java
@@ -22,17 +22,24 @@ public class IndexUnitTest {
 	public void testBuildQueryString() {
 		assertThat(index.buildQueryString("")).isEqualTo("*");
 		assertThat(index.buildQueryString("", "Melville"))
-				.isEqualTo("* AND ( +contribution.agent.label:Melville)");
+				.isEqualTo("* AND (contribution.agent.label:Melville)");
 		assertThat(index.buildQueryString("", "http://d-nb.info/gnd/118580604"))
 				.contains("contribution.agent.id");
 		assertThat(index.buildQueryString("", "", "", "", "", "", "1999-2001"))
-				.isEqualTo("* AND ( +publication.startDate:[1999 TO 2001])");
+				.isEqualTo("* AND (publication.startDate:[1999 TO 2001])");
 		assertThat(index.buildQueryString("", "", "", "", "", "", "1999-*"))
-				.isEqualTo("* AND ( +publication.startDate:[1999 TO *])");
+				.isEqualTo("* AND (publication.startDate:[1999 TO *])");
 		assertThat(index.buildQueryString("", "", "", "", "", "", "*-2001"))
-				.isEqualTo("* AND ( +publication.startDate:[* TO 2001])");
+				.isEqualTo("* AND (publication.startDate:[* TO 2001])");
 		assertThat(index.buildQueryString("", "", "", "", "", "", "*-*"))
-				.isEqualTo("* AND ( +publication.startDate:[* TO *])");
+				.isEqualTo("* AND (publication.startDate:[* TO *])");
+		assertThat(index.buildQueryString("", "", "", "", "123"))
+				.isEqualTo("* AND (isbn:123 OR (issn:123))");
+		assertThat(index.buildQueryString("", "", "", "", "123,456"))
+				.isEqualTo("* AND (isbn:123 AND isbn:456 OR (issn:123 AND issn:456))");
+		assertThat(index.buildQueryString("", "Melville", "", "", "123,456"))
+				.isEqualTo(
+						"* AND (contribution.agent.label:Melville) AND (isbn:123 AND isbn:456 OR (issn:123 AND issn:456))");
 	}
 
 }
