4311f0ad1a232bf6853413baadff0397128aefde
Fabian Steeg <fsteeg@gmail.com>
2017-02-28 12:54:21 +0100

    Update tests for recent data changes

--- web/test/tests/IndexIntegrationTest.java
+++ web/test/tests/IndexIntegrationTest.java
@@ -34,25 +34,25 @@ public class IndexIntegrationTest extends LocalIndexSetup {
 			{ "title:Westfalen", /*->*/ 3 },
 			{ "contribution.agent.label:Westfalen", /*->*/ 7 },
 			{ "contribution.agent.id:\"http\\://d-nb.info/gnd/5265186-1\"", /*->*/ 1 },
-			{ "contribution.agent.id:5265186-1", /*->*/ 3 }, // TODO: don't tokenize at dashes
-			{ "contribution.agent.id:\"5265186-1\"", /*->*/ 1 },
+			{ "contribution.agent.id:5265186-1", /*->*/ 0 },
+			{ "contribution.agent.id:\"5265186-1\"", /*->*/ 0 },
 			{ "title:Westfalen AND contribution.agent.label:Westfalen", /*->*/ 2 },
 			{ "title:Westfalen OR title:Münsterland", /*->*/ 4 },
 			{ "(title:Westfalen OR title:Münsterland) AND contribution.agent.id:\"http\\://d-nb.info/gnd/2019209-5\"", /*->*/ 1 },
 			{ "(title:Westfalen OR title:Münsterland) AND NOT contribution.agent.id:\"http\\://d-nb.info/gnd/2019209-5\"", /*->*/ 4-1 },
-			{ "subject.label:Westfalen", /*->*/ 6 },
+			{ "subject.label:Westfalen", /*->*/ 7 },
 			{ "subject.id:\"http\\://d-nb.info/gnd/4042570-8\"", /*->*/ 2 },
 			{ "subject.id:1113670827", /*->*/ 0 },
 			{ "subject.type:PlaceOrGeographicName", /*->*/ 25 },
-			{ "publication.location:Berlin", /*->*/ 8 },
+			{ "publication.location:Berlin", /*->*/ 9 },
 			{ "publication.startDate:1993", /*->*/ 2 },
 			{ "publication.location:Berlin AND publication.startDate:1993", /*->*/ 1 },
-			{ "publication.location:Berlin AND publication.startDate:[1992 TO 2017]", /*->*/ 7 },
+			{ "publication.location:Berlin AND publication.startDate:[1992 TO 2017]", /*->*/ 8 },
 			{ "collectedBy.id:\"http\\://lobid.org/resources/NWBib\"", /*->*/ 38 },
 			{ "collectedBy.id:NWBib", /*->*/ 0 },
 			{ "publication.publishedBy:Springer", /*->*/ 2 },
-			{ "exemplar.id:\"http\\://lobid.org/items/TT003059252\\:DE-5-58\\:9%2F041#\\!\"", /*->*/ 1 },
-			{ "exemplar.id:TT003059252\\:DE-5-58\\:9%2F041", /*->*/ 0 }
+			{ "hasItem.id:\"http\\://lobid.org/items/TT003059252\\:DE-5-58\\:9%2F041#\\!\"", /*->*/ 1 },
+			{ "hasItem.id:TT003059252\\:DE-5-58\\:9%2F041", /*->*/ 0 }
 		});
 	} // @formatter:on
 
