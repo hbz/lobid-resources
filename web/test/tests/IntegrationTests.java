hbz/lobid-resources

commit 57b473bc4f4b1023289216731fe1de196e7f8a86

Hide temporary labels in API responses
See https://github.com/hbz/lobid-resources/issues/197

web/app/controllers/resources/Index.java
@@ -13,6 +13,7 @@ import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -74,13 +75,20 @@ public class Index {
 	public static Client elasticsearchClient = null;
 
 	/**
-	 * Fields used when building query strings vis
+	 * Fields used when building query strings with
 	 * {@link #buildQueryString(String, String...)}
 	 */
-	public static final String[] FIELDS =
+	public static final String[] QUERY_FIELDS =
 			new String[] { "contribution.agent.label", "title",
 					"subject.id|subject.label", "isbn|issn", "publication.publishedBy",
 					"publication.startDate", "medium.id", "type", "collectedBy.id" };
+
+	/**
+	 * Fields in the index data that should not be included in the response data.
+	 * See https://github.com/hbz/lobid-resources/issues/197
+	 */
+	public static final List<String> HIDE_FIELDS =
+			Arrays.asList("contributorOrder", "subjectOrder", "subjectChain");
 
 	/**
 	 * The values supported for the `aggregations` query parameter.
@@ -91,7 +99,7 @@ public class Index {
 
 	/**
 	 * @param q The current query string
-	 * @param values The values corresponding to {@link #FIELDS}
+	 * @param values The values corresponding to {@link #QUERY_FIELDS}
 	 * @return A query string created from q, expanded for values
 	 */
 	public String buildQueryString(String q, String... values) {
@@ -99,7 +107,7 @@ public class Index {
 		for (int i = 0; i < values.length; i++) {
 			String fieldValue = values[i];
 			String fieldName = fieldValue.contains("http")
-					? FIELDS[i].replace(".label", ".id") : FIELDS[i];
+					? QUERY_FIELDS[i].replace(".label", ".id") : QUERY_FIELDS[i];
 			if (fieldName.toLowerCase().endsWith("date")
 					&& fieldValue.matches("(\\d{1,4}|\\*)-(\\d{1,4}|\\*)")) {
 				String[] fromTo = fieldValue.split("-");
@@ -254,7 +262,17 @@ public class Index {
 	 * @return The index result for the query (the hits) or GET (single result)
 	 */
 	public JsonNode getResult() {
-		return result;
+		return withoutFields(HIDE_FIELDS, result);
+	}
+
+	private static JsonNode withoutFields(List<String> hide, JsonNode json) {
+		JsonNode parent;
+		if (json == null || (parent = json.findParent(hide.get(0))) == null) {
+			return json;
+		}
+		Map<String, Object> map = Json.fromJson(parent, Map.class);
+		hide.forEach(fieldToHide -> map.remove(fieldToHide));
+		return Json.toJson(map);
 	}
 
 	/**

web/test/tests/IntegrationTests.java
@@ -24,6 +24,7 @@ import org.elasticsearch.search.aggregations.bucket.terms.Terms.Bucket;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.base.Joiner;
 
 import controllers.resources.Index;
@@ -90,6 +91,28 @@ public class IntegrationTests extends LocalIndexSetup {
 			Index index = new Index();
 			Long hits = index.totalHits("hbzId:TT050409948");
 			assertThat(hits).isGreaterThan(0);
+		});
+	}
+
+	@Test
+	public void responseJsonFilterGet() {
+		running(testServer(3333), () -> {
+			Index index = new Index();
+			JsonNode hit = index.getResource("TT050409948").getResult();
+			Index.HIDE_FIELDS.forEach(field -> assertThat(hit.get(field)).isNull());
+		});
+	}
+
+	@Test
+	public void responseJsonFilterSearch() {
+		running(testServer(3333), () -> {
+			Index index = new Index();
+			index = index.queryResources("hbzId:TT050409948", 0, 1, "", "", "");
+			assertThat(index.getTotal()).isGreaterThan(0);
+			JsonNode hit = index.getResult();
+			assertThat(hit.findValue("hbzId").asText()).isEqualTo("TT050409948");
+			Index.HIDE_FIELDS
+					.forEach(field -> assertThat(hit.findValue(field)).isNull());
 		});
 	}
 

commit 7c33aae22bc33077d7135cf7ed8247adccae5a68

Fix regression introduced with 57b473b
See https://github.com/hbz/lobid-resources/issues/327

web/app/controllers/resources/Index.java
@@ -262,16 +262,22 @@ public class Index {
 	 * @return The index result for the query (the hits) or GET (single result)
 	 */
 	public JsonNode getResult() {
-		return withoutFields(HIDE_FIELDS, result);
+		return result == null ? null : withoutHiddenFields(result);
 	}
 
-	private static JsonNode withoutFields(List<String> hide, JsonNode json) {
-		JsonNode parent;
-		if (json == null || (parent = json.findParent(hide.get(0))) == null) {
-			return json;
-		}
-		Map<String, Object> map = Json.fromJson(parent, Map.class);
-		hide.forEach(fieldToHide -> map.remove(fieldToHide));
+	private static JsonNode withoutHiddenFields(JsonNode json) {
+		return json.isObject() ? filteredObject(json) : filteredArray(json);
+	}
+
+	private static JsonNode filteredArray(JsonNode json) {
+		List<JsonNode> result = new ArrayList<>();
+		json.elements().forEachRemaining(node -> result.add(filteredObject(node)));
+		return Json.toJson(result);
+	}
+
+	private static JsonNode filteredObject(JsonNode node) {
+		Map<String, Object> map = Json.fromJson(node, Map.class);
+		HIDE_FIELDS.forEach(fieldToHide -> map.remove(fieldToHide));
 		return Json.toJson(map);
 	}
 

web/test/tests/IntegrationTests.java
@@ -13,7 +13,11 @@ import static play.test.Helpers.route;
 import static play.test.Helpers.running;
 import static play.test.Helpers.testServer;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -99,6 +103,8 @@ public class IntegrationTests extends LocalIndexSetup {
 		running(testServer(3333), () -> {
 			Index index = new Index();
 			JsonNode hit = index.getResource("TT050409948").getResult();
+			assertThat(hit.isObject()).as("hit is an object").isTrue();
+			assertThat(hit.findValue("hbzId").asText()).isEqualTo("TT050409948");
 			Index.HIDE_FIELDS.forEach(field -> assertThat(hit.get(field)).isNull());
 		});
 	}
@@ -107,12 +113,20 @@ public class IntegrationTests extends LocalIndexSetup {
 	public void responseJsonFilterSearch() {
 		running(testServer(3333), () -> {
 			Index index = new Index();
-			index = index.queryResources("hbzId:TT050409948", 0, 1, "", "", "");
-			assertThat(index.getTotal()).isGreaterThan(0);
-			JsonNode hit = index.getResult();
-			assertThat(hit.findValue("hbzId").asText()).isEqualTo("TT050409948");
-			Index.HIDE_FIELDS
-					.forEach(field -> assertThat(hit.findValue(field)).isNull());
+			index = index.queryResources("*", 0, 100, "", "", "");
+			assertThat(index.getTotal()).isGreaterThanOrEqualTo(100);
+			JsonNode hits = index.getResult();
+			assertThat(hits.isArray()).as("hits is an array").isTrue();
+			List<JsonNode> nodes = new ArrayList<>();
+			Iterator<JsonNode> elements = hits.elements();
+			elements.forEachRemaining(node -> {
+				Index.HIDE_FIELDS
+						.forEach(field -> assertThat(node.get(field)).as(field).isNull());
+				nodes.add(node);
+			});
+			assertThat(nodes.size()).isGreaterThanOrEqualTo(100);
+			assertThat(new HashSet<>(nodes).size()).as("unique hits")
+					.isEqualTo(nodes.size());
 		});
 	}
 