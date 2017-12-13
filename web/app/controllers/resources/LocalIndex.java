871574f5ee179ed7759053bf4afa945aec315662
Fabian Steeg <steeg@hbz-nrw.de>
2017-12-13 11:13:22 +0100

    Set UTF-8 encoding when building local index
    See https://github.com/hbz/lobid-resources/issues/286

--- web/app/controllers/resources/LocalIndex.java
+++ web/app/controllers/resources/LocalIndex.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -98,7 +99,9 @@ public class LocalIndex {
 			BulkRequestBuilder bulkRequest) {
 		Arrays.asList(sampleDataRoot.listFiles()).forEach((file) -> {
 			try {
-				String data = Files.readAllLines(Paths.get(file.getAbsolutePath()))
+				String data = Files
+						.readAllLines(Paths.get(file.getAbsolutePath()),
+								StandardCharsets.UTF_8)
 						.stream().map(String::trim).collect(Collectors.joining());
 				boolean isItem = file.getName().contains(":");
 				String type = isItem ? "item" : "resource";
