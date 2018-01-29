28b2f613fcf14a961b3735f5249fb8184a4be25f
Fabian Steeg <steeg@hbz-nrw.de>
2018-01-29 14:31:44 +0100

    Add German analyzer to additional fields
    See https://github.com/hbz/nwbib/issues/163

--- web/test/tests/IndexIntegrationTest.java
+++ web/test/tests/IndexIntegrationTest.java
@@ -16,6 +16,7 @@ import org.junit.runners.Parameterized.Parameters;
 
 import controllers.resources.Queries;
 import controllers.resources.Search;
+import play.Logger;
 
 /**
  * Integration tests for functionality provided by the {@link Search} class.
@@ -34,18 +35,25 @@ public class IndexIntegrationTest extends LocalIndexSetup {
 			{ "title:der", /*->*/ 21 },
 			{ "title:Westfalen", /*->*/ 3 },
 			{ "contribution.agent.label:Westfalen", /*->*/ 7 },
+			{ "contribution.agent.label:Westfälen", /*->*/ 7 },
 			{ "contribution.agent.id:\"http\\://d-nb.info/gnd/5265186-1\"", /*->*/ 1 },
 			{ "contribution.agent.id:5265186-1", /*->*/ 0 },
 			{ "contribution.agent.id:\"5265186-1\"", /*->*/ 0 },
 			{ "title:Westfalen AND contribution.agent.label:Westfalen", /*->*/ 2 },
 			{ "title:Westfalen OR title:Münsterland", /*->*/ 4 },
+			{ "title:Westfalen OR title:Munsterland", /*->*/ 4 },
 			{ "(title:Westfalen OR title:Münsterland) AND contribution.agent.id:\"http\\://d-nb.info/gnd/2019209-5\"", /*->*/ 1 },
 			{ "(title:Westfalen OR title:Münsterland) AND NOT contribution.agent.id:\"http\\://d-nb.info/gnd/2019209-5\"", /*->*/ 4-1 },
 			{ "subject.componentList.label:Westfalen", /*->*/ 8 },
+			{ "subject.componentList.label:Westfälen", /*->*/ 8 },
+			{ "subject.label:Westfalen", /*->*/ 13 },
+			{ "subject.label:Westfälen", /*->*/ 13 },
 			{ "subject.componentList.id:\"http\\://d-nb.info/gnd/4042570-8\"", /*->*/ 3 },
 			{ "subject.componentList.id:1113670827", /*->*/ 0 },
 			{ "subject.componentList.type:PlaceOrGeographicName", /*->*/ 28 },
 			{ "publication.location:Berlin", /*->*/ 15 },
+			{ "publication.location:Köln", /*->*/ 4 },
+			{ "publication.location:Koln", /*->*/ 4 },
 			{ "publication.startDate:1993", /*->*/ 3 },
 			{ "publication.location:Berlin AND publication.startDate:1993", /*->*/ 1 },
 			{ "publication.location:Berlin AND publication.startDate:[1992 TO 2017]", /*->*/ 12 },
@@ -53,6 +61,8 @@ public class IndexIntegrationTest extends LocalIndexSetup {
 			{ "inCollection.id:NWBib", /*->*/ 0 },
 			{ "publication.publishedBy:Springer", /*->*/ 4 },
 			{ "publication.publishedBy:Spring", /*->*/ 4 },
+			{ "publication.publishedBy:DAG", /*->*/ 1 },
+			{ "publication.publishedBy:DÄG", /*->*/ 1 },
 			{ "hasItem.id:\"http\\://lobid.org/items/TT003059252\\:DE-5-58\\:9%2F041#\\!\"", /*->*/ 1 },
 			{ "hasItem.id:TT003059252\\:DE-5-58\\:9%2F041", /*->*/ 0 }
 		});
@@ -70,7 +80,9 @@ public class IndexIntegrationTest extends LocalIndexSetup {
 	@Test
 	public void testResultCount() {
 		running(fakeApplication(), () -> {
-			assertThat(index.totalHits()).isEqualTo(expectedResultCount);
+			long totalHits = index.totalHits();
+			Logger.debug("{}", index.getResult());
+			assertThat(totalHits).isEqualTo(expectedResultCount);
 		});
 	}
 
