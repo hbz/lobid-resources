5b1cf8b122efd0eb3580b0264a7e3cff15a98404
Pascal Christoph <christoph@hbz-nrw.de>
2018-02-13 15:23:49 +0100

    Provide gnd beacon
    Get the data from lobid. Create a beacon.
    
    See #685.

---
+++ src/main/resources/beacon/makePndBeacon.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail # See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+
+cd data
+
+cp beacon_head.txt hbzlod-pndbeacon.txt
+
+curl -L "http://lobid.org/resources/search?q=%28describedBy.dateModified%3A%3E20170726+OR+describedBy.dateCreated%3A%3E20170726%29+AND+contribution.agent.id%3A*&format=bulk"  > resources-from-20170726.jsonl 
+
+cat *.jsonl | jq -r '. | select(.contribution != null) | .contribution[].agent.id'  | sort | uniq -c | sort -n -r | sed 's#\(.*\)http://d-nb.info/gnd/\(.*\)#\2|\1#g' | tr -d ' '|grep -v null  >> hbzlod-pndbeacon.txt
+
+scp hbzlod-pndbeacon.txt lobid@emphytos:/usr/local/lobid/src/lobid.org/download/beacons/
