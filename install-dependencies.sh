a376b34b272a2b67a3728acea7884b7e30baee21
Pascal Christoph <christoph@hbz-nrw.de>
2019-03-19 14:56:26 +0100

    Use metafacture 5 master branch
    Resolves #954.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,8 +3,6 @@ cd ../..
 git clone https://github.com/hbz/metafacture-core.git
 cd metafacture-core
 git pull
-git checkout 5.0.1-hbz
-git pull
 ./gradlew install
 cd ..
 git clone https://github.com/jsonld-java/jsonld-java.git
