9e8c42b876644b408a8ed671c6b72a9fd160dbaf
Pascal Christoph <christoph@hbz-nrw.de>
2020-09-07 15:49:24 +0200

    Use newest full dump
    Works on weywot1 where the data resides.
    
    - add some description
    
    See #1075.

--- src/main/resources/beacon/makePndBeacon.sh
+++ src/main/resources/beacon/makePndBeacon.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
+# see https://github.com/hbz/lobid-resources/issues/1075
+
 set -euo pipefail # See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+RESOURCES_DIRECTORY=/data/DE-605/resources
 
 cd data
-TIMESTAMP=$(date +%Y-%m-%d)
-REVISIT=$(date +%Y-%m-%d --date="$TIMESTAMP  +1 month")
-
-sed -i "s/#REVISIT.*/#REVISIT: $REVISIT/g" beacon_head.txt
-sed -i "s/#TIMESTAMP.*/#TIMESTAMP: $TIMESTAMP/g" beacon_head.txt
 
 cp beacon_head.txt hbzlod-pndbeacon.txt
 
-curl -L "http://lobid.org/resources/search?q=%28describedBy.dateModified%3A%3E20170726+OR+describedBy.dateCreated%3A%3E20170726%29+AND+contribution.agent.id%3A*&format=bulk"  > resources-from-20170726.jsonl 
+curl -L "http://lobid.org/resources/search?q=%28describedBy.dateModified%3A%3E20200615+OR+describedBy.dateCreated%3A%3E20200615%29+AND+contribution.agent.id%3A*&format=bulk"  > $RESOURCES_DIRECTORY/resources-from-20200615.jsonl
 
-cat *.jsonl | jq -r '. | select(.contribution != null) | .contribution[].agent.id'  | grep -v '_:b' | sort | uniq -c | sort -n -r | sed 's#\(.*\)https://d-nb.info/gnd/\(.*\)#\2|\1#g' | tr -d ' '|grep -v null  >> hbzlod-pndbeacon.txt
+cat $RESOURCES_DIRECTORY/*.jsonl | jq -r '. | select(.contribution != null) | .contribution[].agent.id'  | grep -v '_:b' | sort | uniq -c | sort -n -r | sed 's#\(.*\)https://d-nb.info/gnd/\(.*\)#\2|\1#g' | tr -d ' '|grep -v null  >> hbzlod-pndbeacon.txt
 
 scp hbzlod-pndbeacon.txt lobid@emphytos:/usr/local/lobid/src/lobid.org/download/beacons/
