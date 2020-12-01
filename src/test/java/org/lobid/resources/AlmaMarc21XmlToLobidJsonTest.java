aa7bfc72ad556e57c65b80be5a6680354298f37f
Pascal Christoph <christoph@hbz-nrw.de>
2020-12-01 14:35:09 +0100

    Cleanup code; use Logger instead of Sytem.out; add comments

--- src/test/java/org/lobid/resources/AlmaMarc21XmlToLobidJsonTest.java
+++ src/test/java/org/lobid/resources/AlmaMarc21XmlToLobidJsonTest.java
@@ -15,6 +15,9 @@ import java.util.HashMap;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.metafacture.biblio.marc21.MarcXmlHandler;
@@ -37,9 +40,11 @@ public final class AlmaMarc21XmlToLobidJsonTest {
   private static final File DIRECTORY = new File("src/test/resources/alma/");
   private static final String XML = "xml";
   final HashMap<String, String> morphVariables = new HashMap<>();
-  private static boolean GENERATE_TESTDATA =
+  private static final boolean GENERATE_TESTDATA =
       System.getProperty("generateTestData", "false").equals("true");
   private static final PrintStream ORIG_OUT = System.out;
+  private static final Logger LOG =
+      LogManager.getLogger(Hbz01MabXmlEtlNtriples2Filesystem.class);
 
   /**
    * Sets necessary morph variables.
@@ -49,12 +54,22 @@ public final class AlmaMarc21XmlToLobidJsonTest {
     morphVariables.put("isil", "DE-632");
     morphVariables.put("member", "DE-605");
     morphVariables.put("catalogid", "DE-605");
-    GENERATE_TESTDATA = false;
+  }
+
+  /**
+   * Cleans a bit up. Sets the System.out to the original PrintStream.
+   */
+  @After
+  public void cleanup() {
+    System.setOut(ORIG_OUT);
   }
 
   /**
    * Tests files from the test directory, one per one. Ignore the dynamically
    * created "endTime".
+   *
+   * If the System-Property "generateTestData" is set to true the generated data
+   * is written into files and thus will act as the new expected data.
    */
   @Test
   public void transformFiles() {
@@ -65,19 +80,19 @@ public final class AlmaMarc21XmlToLobidJsonTest {
           ObjectMapper mapper = new ObjectMapper();
           final String filenameJson =
               file.getAbsolutePath().replaceAll("\\." + XML, "\\.json");
-          System.out.println(filenameJson);
           try {
             JsonNode expectedJsonNode = mapper.readTree(new File(filenameJson));
             Object expectedJsonObject =
                 mapper.readValue(expectedJsonNode.toString(), Object.class);
             String expectedJson = mapper.writerWithDefaultPrettyPrinter()
                 .writeValueAsString(expectedJsonObject);
-
             FileOpener opener = new FileOpener();
+
             opener.setReceiver(new XmlDecoder())
                 .setReceiver(new MarcXmlHandler())
                 .setReceiver(new Metamorph(MORPH, morphVariables))
                 .setReceiver(jsonEncoder);
+
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             PrintStream ps = new PrintStream(baos);
             System.setOut(ps);
@@ -89,19 +104,14 @@ public final class AlmaMarc21XmlToLobidJsonTest {
             opener.process(file.getAbsolutePath());
             opener.closeStream();
             if (!GENERATE_TESTDATA) {
-              mapper = new ObjectMapper();// brauchen
-                                          // wir
-                                          // das
-                                          // ?
               String actualJson = null;
               actualJson = baos.toString(StandardCharsets.UTF_8);
-              System.setOut(ORIG_OUT);
-              System.out.println(actualJson);
-              System.out.println(expectedJson);
+              LOG.debug(actualJson);
               // don't test the dynamically created "endTime", e.g:
               // "endTime":"2020-11-30T10:03:42",
               assertEquals(expectedJson.replaceFirst("endTime.{25}", ""),
-                  actualJson.replaceFirst("endTime.{25}", ""));
+                  actualJson.substring(0, actualJson.length() - 1)
+                      .replaceFirst("endTime.{25}", ""));
             }
           } catch (Exception e) {
             e.printStackTrace();
