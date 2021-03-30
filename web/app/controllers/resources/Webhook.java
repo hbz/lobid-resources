20d54a718a08a3262d13693c9519876060af9c73
Pascal Christoph <christoph@hbz-nrw.de>
2021-03-30 10:46:05 +0200

    Fix timestamp on index
    - refactor logging
    
    See #1159.

--- web/app/controllers/resources/Webhook.java
+++ web/app/controllers/resources/Webhook.java
@@ -6,7 +6,8 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import org.lobid.resources.run.AlmaMarcXml2lobidJsonEs;
 import play.Logger;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import de.hbz.lobid.helper.Email;
 
 /**
@@ -29,11 +30,14 @@ public class Webhook extends Controller {
   private static final String INDEX_BASEDUMP_ALIAS_SUFFIX = "-staging";
   private static final String UPDATE_NEWEST_INDEX = "exact";
   private static final String CREATE_INDEX = "create";
-  private static final String CREATE_INDEX_NAME = INDEX_NAME + "-" + LocalDate.now();
+  private static final String CREATE_INDEX_NAME = INDEX_NAME + "-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-kkmm"));
   private static final String MORPH_FILENAME = "alma.xml";
   // If null, create default values from Global settings
   public static String clusterHost = null;
   public static String clusterName = null;
+  private static String msgWrongToken = "'%s' is the wrong token. Declining to ETL %s.";
+  private static String msgStartEtl = "Starting ETL of '%s'...";
+  private static String msgEtlFailed = "ETL of '%s' failed: %s";
 
   /**
    * Triggers ETL of updates.
@@ -42,23 +46,21 @@ public class Webhook extends Controller {
    * @return 200 ok or 403 forbidden response (depending on token) or 500 in
    *         case of internal server error
    */
-  public static Result updateAlma(final String token) {
-    if (!token.equals(TOKEN)) {
-      sendMail("update", false, token + " is the wrong token");
-      return forbidden("Wrong token. Declining to etl update.");
+  public static Result updateAlma(final String GIVEN_TOKEN) {
+    final String KIND="update";
+    if (!GIVEN_TOKEN.equals(TOKEN)) {
+      return wrongToken(KIND, GIVEN_TOKEN);
     }
     try {
-      Logger.info("Starting ETL of updates ...");
+      Logger.info(String.format(msgStartEtl, KIND));
       AlmaMarcXml2lobidJsonEs.main(FILENAME_UPDATE, INDEX_NAME,
       INDEX_UPDATE_ALIAS_SUFFIX, clusterHost, clusterName, UPDATE_NEWEST_INDEX,
           MORPH_FILENAME);
     } catch (Exception e) {
-      Logger.error("Transformation failed", e);
-      sendMail("update", false, e.toString());
-      return internalServerError("ETL of update failed:" +e.toString());
+        return etlFailed(KIND, e.toString());
     }
-    sendMail("update", true, ". Updated index with name "+ INDEX_NAME);
-    return ok("... finished ETL update!");
+    sendMail(KIND, true, "Updated index '"+ INDEX_NAME + "'");
+    return ok("... finished ETL " + KIND);
   }
 
  /**
@@ -68,30 +70,42 @@ public class Webhook extends Controller {
    * @return 200 ok or 403 forbidden response (depending on token) or 500 in
    *         case of internal server error
    */
-  public static Result basedumpAlma(final String token) {
-    if (!token.equals(TOKEN)) {
-      sendMail("basedump", false, token + " is the wrong token");
-      return forbidden("Wrong token. Declining to etl basedump.");
+  public static Result basedumpAlma(final String GIVEN_TOKEN) {
+    final String KIND="basedump";
+    if (!GIVEN_TOKEN.equals(TOKEN)) {
+      return wrongToken(KIND, GIVEN_TOKEN);
     }
     try {
-      Logger.info("Starting ETL of basedump ...");
+      Logger.info(String.format(msgStartEtl, KIND));
       AlmaMarcXml2lobidJsonEs.main(FILENAME_BASEDUMP, CREATE_INDEX_NAME,
       INDEX_BASEDUMP_ALIAS_SUFFIX, clusterHost, clusterName, CREATE_INDEX,
           MORPH_FILENAME);
     } catch (Exception e) {
-      Logger.error("Transformation failed", e);
-      sendMail("basedump", false, e.toString());
-      return internalServerError("ETL of basedump failed:" + e.toString());
+        return etlFailed(KIND, e.toString());
     }
-    sendMail("basedump", true, ". Created new index with name "+ INDEX_NAME + " , adding " + INDEX_BASEDUMP_ALIAS_SUFFIX +" to alias of index");
-    return ok("... finished ETL basedump!");
+    sendMail(KIND, true, "Created new index with name "+ CREATE_INDEX_NAME + " , adding " + INDEX_BASEDUMP_ALIAS_SUFFIX +" to alias of index");
+    return ok("... finished ETL " + KIND);
   }
 
-  private static void sendMail(final String kind, final boolean success, final String message) {
+  private static Result wrongToken (final String KIND, final String TOKEN) {
+    String msg = String.format(msgWrongToken, TOKEN, KIND);
+    Logger.error(msg);
+    sendMail(KIND, false, msg);
+    return forbidden(msg);
+  }
+
+  private static Result etlFailed (final String KIND, final String ERROR) {
+    String msg = String.format(msgEtlFailed, KIND, ERROR);
+    Logger.error(msg);
+    sendMail(KIND, false, msg);
+    return internalServerError(msg);
+  }
+
+  private static void sendMail(final String KIND, final boolean SUCCESS, final String MESSAGE) {
     try {
-    Email.sendEmail("hduser", EMAIL, "Webhook ETL of " + kind + " " + (success ? "success :)" : "fails :("), message);
+      Email.sendEmail("hduser", EMAIL, "Webhook ETL of " + KIND + " " + (SUCCESS ? "success :)" : "fails :("), MESSAGE);
     } catch (Exception e) {
-      Logger.error("Couldn't send email", e.toString());
+        Logger.error("Couldn't send email", e.toString());
     }
   }
 
