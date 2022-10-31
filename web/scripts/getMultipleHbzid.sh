97edd442892d26788ed1b93e0fe2e30237d600a4
Pascal Christoph <christoph@hbz-nrw.de>
2022-10-31 13:05:39 +0100

    Filter also almaMmsId; fix formatting in mail (#1459)

--- web/scripts/getMultipleHbzid.sh
+++ web/scripts/getMultipleHbzid.sh
@@ -9,13 +9,14 @@ SEND_TO=$(cat .secrets/SEND_TO)
 cd ../
 NEWEST_LOG_FN=$(ls application-log*.gz| tail -n1)
 echo $NEWEST_LOG_FN
-HBZIDS=$(zgrep -B2 'java.lang.IllegalStateException: Expected String, got Array' $NEWEST_LOG_FN|grep -B1 'replace_all("hbzId",' |grep hbzId | sed "s#\(.*\)hbzId\(.*\)#\2#g"|grep =|sed "s#=\[\(.*\)\].*#\1#g")
+HBZIDS=$( zgrep -B2 'java.lang.IllegalStateException: Expected String, got Array' $NEWEST_LOG_FN |grep -B1 'replace_all("hbzId",'| grep hbzId= | sed "s#\(.*\)almaMmsId=\(.*\), id=.*hbzId\(.*\)#\2\3#g" |tr = , |sed "s#\[\(.*\)\].*#\1#g"|sort -u)
+
 if [ -n "$HBZIDS" ]; then
        echo "non null: $HBZIDS"
        mail -s "Multiple hbzId entries in Alma" "${MAIL_TO}" -a "From: ${MAIL_FROM}" << EOF
        Triggered by crontab@weywot1
        To be send to ${SEND_TO}:
-       $HBZIDS
+$HBZIDS
 EOF
 fi
 
