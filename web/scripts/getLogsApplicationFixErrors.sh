c7680d39e88d4ea5d92245465d9284910e8f9e27
Pascal Christoph <christoph@hbz-nrw.de>
2022-11-07 14:59:20 +0100

    Fix variable name
    Mail body was empty if size of errors not big.

--- web/scripts/getLogsApplicationFixErrors.sh
+++ web/scripts/getLogsApplicationFixErrors.sh
@@ -14,14 +14,14 @@ if [ -n "$ERRORS" ]; then
 	ERRORS_LINES_COUNT=$(echo "${ERRORS}"|wc -l)
 	if [ ${chrlen} -gt 500000 ]; then
 		ERRORS_TAILED="Es sind zu viele ERRORS - es werden nur die letzten 30 Zeilen angezeigt von insgesamt $ERRORS_LINES_COUNT:"
-		ERRORS_TAILED="$ERRORS_TAILED $(echo "$ERRORS"|tail -n30)"
+		ERRORS="$ERRORS_TAILED $(echo "$ERRORS"|tail -n30)"
 	fi
        mail -s "FIX errors in Alma Fix ETL" "${MAIL_TO}" -a "From: ${MAIL_FROM}" << EOF
 Getriggert von ausgeführt in $(pwd)/scripts :
 
 Achte auf das Datum der ERROR-Zeilen - evtl. sind das alte Fehler!
 
-$ERRORS_TAILED
+$ERRORS
 EOF
 fi
 
