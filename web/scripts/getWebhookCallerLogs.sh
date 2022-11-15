a5555d4fef422e3b20f68e6b85fe727fc95fb169
Pascal Christoph <christoph@hbz-nrw.de>
2022-11-15 16:35:18 +0100

    Mail when triggering Webhook is not sucessful (#1512)

---
+++ web/scripts/getWebhookCallerLogs.sh
@@ -0,0 +1,22 @@
+# author: dr0i
+#
+# grep all logs concerning the triggering of the Webhook in ETL.log , filtered
+# to today's entries, and send email if ERRORS appeared or response code != 204
+# see https://github.com/hbz/lobid-resources/issues/1512
+
+MAIL_TO=$(cat .secrets/MAIL_TO)
+MAIL_FROM=$(cat .secrets/MAIL_FROM)
+
+ERROR_PATTERN="Got response code\|HttpPoster\|notifyWebhook"
+
+NEWEST_LOG_FN="../logs/ETL.log"
+ERRORS=$(grep  -B1 "$ERROR_PATTERN" $NEWEST_LOG_FN)
+DATE=$(date +"%F")
+ERRORS=$(echo "$ERRORS" | grep $DATE| grep "ERROR\|response code"|grep -v 204)
+
+if [ -n "$ERRORS" ]; then
+        mail -s "Triggered Webhook's response in Alma Fix ETL" "${MAIL_TO}" -a "From: ${MAIL_FROM}" << EOF
+$ERRORS
+EOF
+fi
+
