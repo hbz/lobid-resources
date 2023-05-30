ccb205cbf0c6dbc339cfa75b3ea5c18d320baaf9
Pascal Christoph <christoph@hbz-nrw.de>
2023-05-30 14:33:29 +0200

    Adjust json path to the data (#1710)
    - fix the date pattern
    - fix the beacon header

--- src/main/resources/beacon/makePndBeacon.sh
+++ src/main/resources/beacon/makePndBeacon.sh
@@ -8,7 +8,7 @@ cd data
 
 cp beacon_head.txt hbzlod-pndbeacon.txt
 
-curl -L "http://lobid.org/resources/search?q=%28describedBy.dateModified%3A%3E20200615+OR+describedBy.dateCreated%3A%3E20200615%29+AND+contribution.agent.id%3A*&format=bulk"  > $RESOURCES_DIRECTORY/resources-from-20200615.jsonl
+curl -L "http://lobid.org/resources/search?q=%28describedBy.resultOf.object.dateModified%3A%3E2020-06-15+OR+describedBy.resultOf.object.dateCreated%3A%3E2020-06-15%29+AND+contribution.agent.id%3A*&format=bulk"  > $RESOURCES_DIRECTORY/resources-from-20200615.jsonl
 
 cat $RESOURCES_DIRECTORY/*.jsonl | jq -r '. | select(.contribution != null) | .contribution[].agent.id'  | grep -v '_:b' | sort | uniq -c | sort -n -r | sed 's#\(.*\)https://d-nb.info/gnd/\(.*\)#\2|\1#g' | tr -d ' '|grep -v null  >> hbzlod-pndbeacon.txt
 
