96ed0b1fc7c23c1d365f395c1ab9b4d874af9392
Pascal Christoph <christoph@hbz-nrw.de>
2023-11-27 09:03:49 +0100

    Fix path in logs

--- web/scripts/getLogsEtlFixErrors.sh
+++ web/scripts/getLogsEtlFixErrors.sh
@@ -21,7 +21,7 @@ if [ -n "$ERRORS" ]; then
         fi
 	echo "$SHORTEND_ERRORS"
         mail -s "FIX errors in Alma Fix ETL" "${MAIL_TO}" -a "From: ${MAIL_FROM}" << EOF
-Getriggert von ausgeführt in $(pwd)/scripts/getLogsEtlFixErrors.sh :
+Getriggert von ausgeführt in $(pwd)/getLogsEtlFixErrors.sh :
 
 Achte auf das Datum der ERROR-Zeilen - evtl. sind das alte Fehler!
 
