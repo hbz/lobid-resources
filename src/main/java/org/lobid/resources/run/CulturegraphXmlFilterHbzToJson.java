848c948be5d99e24d992aada952d4957826db5a5
Pascal Christoph <christoph@hbz-nrw.de>
2024-05-28 15:31:03 +0200

    Reduce complexity of flow (#1058)
    Follows https://github.com/metafacture/metafacture-examples/pull/8.

--- src/main/java/org/lobid/resources/run/CulturegraphXmlFilterHbzToJson.java
+++ src/main/java/org/lobid/resources/run/CulturegraphXmlFilterHbzToJson.java
@@ -7,43 +7,39 @@ import java.io.IOException;
 
 import org.metafacture.biblio.marc21.MarcXmlHandler;
 import org.metafacture.elasticsearch.JsonToElasticsearchBulk;
-import org.metafacture.flowcontrol.ObjectThreader;
 import org.metafacture.io.FileOpener;
 import org.metafacture.io.ObjectWriter;
 import org.metafacture.json.JsonEncoder;
-import org.metafacture.mangling.LiteralToObject;
-import org.metafacture.strings.StringReader;
 import org.metafacture.xml.XmlDecoder;
-import org.metafacture.xml.XmlElementSplitter;
 import org.metafacture.metafix.Metafix;
 
 /**
- * Filter resources with hbz holdings from culturegraph marcxml while tranform it with reject()
+ * Filter resources with hbz holdings from culturegraph's MARCXML while tranform it with reject()
  * into JSON and write this as an elasticsearch bulk json file.
  *
  * @author Pascal Christoph (dr0i)
  * @author Tobias Bülte (TobiasNx)
  **/
-@SuppressWarnings("javadoc")
 public final class CulturegraphXmlFilterHbzToJson {
 	private static final String ELASTICSEARCH_INDEX_NAME = "cg";
+	public static final String ELASTICSEARCH_INDEX_TYPE_NAME="rvk";
 	private static String JSON_FILE="bulk.ndjson";
-	private static final String XML_SPLITTER_ELEMENT = "record";
-	private static final String XML_SPLITTER_TOP_ELEMENT = "marc:collection";
 
 	public static void main(String... args) {
-		String XML_INPUT_FILE =new File(args[0]).getAbsolutePath();
+		String XML_INPUT_FILE = new File(args[0]).getAbsolutePath();
 
-		if (args.length >1) JSON_FILE=args[1];
+		if (args.length > 1) JSON_FILE = args[1];
 
 		final FileOpener opener = new FileOpener();
-		opener.setReceiver(new XmlDecoder())
-				.setReceiver(new XmlElementSplitter(XML_SPLITTER_TOP_ELEMENT,
-						XML_SPLITTER_ELEMENT)) //
-				.setReceiver(new LiteralToObject())
-				.setReceiver(new ObjectThreader<String>())//
-				.addReceiver(receiverThread()); // one thread for it's working
-												// on one file
+		try {
+			opener.setReceiver(new XmlDecoder()).setReceiver(new MarcXmlHandler())
+					.setReceiver(new Metafix("src/main/resources/fix-cg-to-es.fix"))
+					.setReceiver(new JsonEncoder())
+					.setReceiver(new JsonToElasticsearchBulk(ELASTICSEARCH_INDEX_TYPE_NAME, ELASTICSEARCH_INDEX_NAME))
+					.setReceiver(new ObjectWriter<>(JSON_FILE));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		opener.process(
 				new File(XML_INPUT_FILE).getAbsolutePath());
 		try {
@@ -51,21 +47,5 @@ public final class CulturegraphXmlFilterHbzToJson {
 		} catch (final NullPointerException e) {
 			// ignore, see https://github.com/hbz/lobid-resources/issues/1030
 		}
-	}
-
-	private static StringReader receiverThread() {
-		final StringReader sr = new StringReader();
-		try {
-            sr.setReceiver(new XmlDecoder()).setReceiver(new MarcXmlHandler())
-            		.setReceiver(
-            				new Metafix("src/main/resources/fix-cg-to-es.fix"))
-            		.setReceiver(new JsonEncoder())
-            		.setReceiver(new JsonToElasticsearchBulk("rvk",
-            				ELASTICSEARCH_INDEX_NAME))
-            		.setReceiver(new ObjectWriter<>(JSON_FILE));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-		return sr;
 	}
 }
