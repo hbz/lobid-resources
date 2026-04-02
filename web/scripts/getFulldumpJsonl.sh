430d62e711af1ba3367054647ae50794832d821c
Pascal Christoph <christoph@hbz-nrw.de>
2026-04-02 11:12:37 +0200

    Keep only 3 fulldumps (#2252)

--- web/scripts/getFulldumpJsonl.sh
+++ web/scripts/getFulldumpJsonl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # see https://github.com/hbz/lobid-resources/issues/2252
 # should be scheduled after weekly ETL
-# If bulk download not > 20 GB the filename ois prefixed with "broken".
-# Removes old data (4 weeks aka files are enough).
+# If bulk download not > 20 GB the filename is prefixed with "broken".
+# Removes old data (3 fulldumps kept are enough).
 # Creates a symlink "latestLobidResources.jsonl.gz" to the latest bulk file.:
 
 # Deliberately not setting this because we dont' want to just bail out, e.g. when
@@ -18,14 +18,12 @@ FULLDUMP_FNAME=${DATE}${FULLDUMP_FNAME_SUFFIX}
 FULLDUMP_DIR="/data/DE-605/resources"
 
 function rmOldData {
-        if [ $(ls ${FULLDUMP_DIR}/*${FULLDUMP_FNAME_SUFFIX}| wc -l) -gt 4 ]; then
-                echo "more than 4 fulldumps - deleting oldest and test again ..."
+        if [ $(ls ${FULLDUMP_DIR}/*${FULLDUMP_FNAME_SUFFIX}| wc -l) -gt 3 ]; then
+                echo "more than 3 fulldumps - deleting oldest and test again ..."
                 rm -f $(ls ${FULLDUMP_DIR}/*${FULLDUMP_FNAME_SUFFIX} |head -n1)
                 rmOldData
         fi
 }
-
-rmOldData
 
 echo "Start: get fulldump at $(date)"
 # get fulldump
@@ -39,6 +37,9 @@ if [[ $(find ${FULLDUMP_FNAME} -type f -size +19G 2>/dev/null) ]]; then
 fi
 
 mv ${FULLDUMP_FNAME} ${FULLDUMP_DIR}/
+
+rmOldData
+
 cd  ${FULLDUMP_DIR}
 # update symlink to the latest dump
 rm latestLobidResources.jsonl.gz
